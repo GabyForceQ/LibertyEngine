c0ed01bf05ae699b4a3a3b45a166479930486418
gabriel.gheorghe <knoppy273@live.com>
2018-08-25 13:18:45 +0300

    Project structure 8.0b - phase.2

--- source/liberty/core/system/surface.d
+++ source/liberty/core/system/surface.d
@@ -2,20 +2,34 @@
  * Copyright:       Copyright (C) 2018 Gabriel Gheorghe, All Rights Reserved
  * Authors:         $(Gabriel Gheorghe)
  * License:         $(LINK2 https://www.gnu.org/licenses/gpl-3.0.txt, GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007)
- * Source:          $(LINK2 https://github.com/GabyForceQ/LibertyEngine/blob/master/source/liberty/core/system/surface/impl.d, _impl.d)
+ * Source:          $(LINK2 https://github.com/GabyForceQ/LibertyEngine/blob/master/source/liberty/core/system/surface.d, _surface.d)
  * Documentation:
  * Coverage:
 **/
-module liberty.core.system.surface.impl;
+module liberty.core.system.surface;
 
-import liberty.core.system.surface.wrapper.types : 
-  SurfaceHandler, 
-  PixelFormat, 
-  Rect;
+import derelict.sdl2.sdl :
+  SDL_Rect,
+  SDL_Surface,
+  SDL_PixelFormat,
+  SDL_FreeSurface,
+  SDL_CreateRGBSurface, 
+  SDL_CreateRGBSurfaceFrom,
+  SDL_ConvertSurface,
+  SDL_LockSurface, 
+  SDL_UnlockSurface,
+  SDL_GetError, 
+  SDL_ClearError,
+  SDL_BlitSurface, 
+  SDL_BlitScaled,
+  SDL_GetRGBA, 
+  SDL_MapRGBA, 
+  SDL_SetColorKey,
+  SDL_TRUE,
+  SDL_FALSE;
 
 import liberty.core.system.constants : Owned;
 import liberty.core.system.platform : Platform;
-import liberty.core.system.surface.wrapper.util : SurfaceUtil;
 import liberty.core.logger.manager : Logger;
 import liberty.graphics.color : Color;
 
@@ -24,7 +38,7 @@ import liberty.graphics.color : Color;
 **/
 final class Surface {
   private {
-    SurfaceHandler _surfaceHandle;
+    SDL_Surface* _surfaceHandle;
     Platform _platform;
     Owned _handleOwned;
     bool _locked;
@@ -35,7 +49,7 @@ final class Surface {
   **/
   this(
     Platform platform, 
-    SurfaceHandler surface, 
+    SDL_Surface* surface, 
     Owned owned
   ) pure @trusted
   in {
@@ -57,12 +71,22 @@ final class Surface {
     Color mask
   ) @trusted {
     _platform = platform;
-    _surfaceHandle = SurfaceUtil.self.createRGBSurface(
+    _surfaceHandle = SDL_CreateRGBSurface(
+      0, 
       width, 
       height, 
       depth, 
-      mask
+      mask.r, 
+      mask.g, 
+      mask.b, 
+      mask.a
     );
+    if (_surfaceHandle is null) {
+      Logger.self.error(
+        "Failed to create the RGB surface",
+        typeof(this).stringof
+      );
+    }
     _handleOwned = Owned.Yes;
   }
 
@@ -79,23 +103,30 @@ final class Surface {
     Color mask
   ) @trusted {
     _platform = platform;
-    _surfaceHandle = SurfaceUtil.self.createRGBSurfaceFrom(
+    _surfaceHandle = SDL_CreateRGBSurfaceFrom(
       pixels, 
       width, 
       height, 
       depth, 
       pitch, 
-      mask
+      mask.r, 
+      mask.g, 
+      mask.b, 
+      mask.a
     );
+    if (_surfaceHandle is null) {
+      Logger.self.error(
+        "Failed to create the RGB surface",
+        typeof(this).stringof
+      );
+    }
     _handleOwned = Owned.Yes;
   }
 
   ~this() @trusted {
     if (_surfaceHandle !is null) {
       if (_handleOwned == Owned.Yes) {
-        SurfaceUtil.self.freeSurface(
-          _surfaceHandle
-        );
+        SDL_FreeSurface(_surfaceHandle);
       }
       _surfaceHandle = null;
     }
@@ -104,14 +135,19 @@ final class Surface {
   /**
    *
   **/
-  Surface convert(
-    const PixelFormat newFormat
-  ) @trusted {
+  Surface convert(const SDL_PixelFormat* newFormat) @trusted {
     // Covert the surface internally
-    SurfaceHandler surface = SurfaceUtil.self.convertSurface(
-      _surfaceHandle, 
-      newFormat
+    SDL_Surface* surface = SDL_ConvertSurface(
+      _surfaceHandle,
+      newFormat,
+      0
     );
+    if (surface is null) {
+      Logger.self.error(
+        "Failed to convert the surface to the new format",
+        typeof(this).stringof
+      );
+    }
 
     // Surface shoudn't be equal with this handle
     if (surface != _surfaceHandle) {
@@ -164,7 +200,13 @@ final class Surface {
    * Lock the surface.
   **/
   void lock() @trusted {
-    SurfaceUtil.self.lockSurface(_surfaceHandle);
+    const state = SDL_LockSurface(_surfaceHandle);
+    if (state != SDL_FALSE) {
+      Logger.self.error(
+        "Failed to lock the surface",
+        typeof(this).stringof
+      );
+    }
     _locked = true;
   }
 
@@ -172,24 +214,27 @@ final class Surface {
    * Unlock the surface.
   **/
   void unlock() @trusted {
-    SurfaceUtil.self.unlockSurface(_surfaceHandle);
+    const state = SDL_UnlockSurface(_surfaceHandle);
+    if (state != SDL_FALSE) {
+      Logger.self.error(
+        "Failed to unlock the surface",
+        typeof(this).stringof
+      );
+    }
     _locked = false;
   }
 
   /**
    * Returns surface pixel format.
   **/
-  PixelFormat getPixelFormat() pure nothrow @safe {
+  SDL_PixelFormat* getPixelFormat() pure nothrow @safe {
     return _surfaceHandle.format;
   }
 
   /**
    *
   **/
-  Color rgba(
-    int x, 
-    int y
-  ) @trusted {
+  Color rgba(int x, int y) @trusted {
     // x should be in interval [0, width)
     if (x < 0 || x >= getWidth()) {
       assert(0, "Out of image!");
@@ -201,17 +246,20 @@ final class Surface {
     }
 
     // Init data
-    PixelFormat format = _surfaceHandle.format;
+    SDL_PixelFormat* format = _surfaceHandle.format;
     ubyte* pixels = cast(ubyte*)_surfaceHandle.pixels;
     immutable int pitch = _surfaceHandle.pitch;
     uint* pixel = cast(uint*)(pixels + y * pitch + x * format.BytesPerPixel);
     Color mask;
 
     // Fill color channels with values.
-    SurfaceUtil.self.doGetRGBA(
-      pixel,
-      format,
-      mask
+    SDL_GetRGBA(
+      *pixel, 
+      format, 
+      &mask.r, 
+      &mask.g, 
+      &mask.b, 
+      &mask.a
     );
 
     // Return the new color
@@ -221,49 +269,55 @@ final class Surface {
   /**
    *
   **/
-  void setColorKey(
-    bool enabled,
-    uint key
-  ) @trusted {
-    SurfaceUtil.self.setColorKey(
-      _surfaceHandle,
-      enabled,
+  void setColorKey(bool enabled,uint key) @trusted {
+    const state = SDL_SetColorKey(
+      _surfaceHandle, 
+      enabled ? SDL_TRUE : SDL_FALSE, 
       key
     );
+    if (state != SDL_FALSE) {
+      Logger.self.error(
+        "Failed to set color key",
+        typeof(this).stringof
+      );
+    }
   }
 
   /**
    *
   **/
-  void setColorKey(
-    bool enabled, 
-    Color mask
-  ) @trusted {
-    const key = SurfaceUtil.self.mapRGBA(
-      _surfaceHandle.format,
-      mask
+  void setColorKey(bool enabled, Color mask) @trusted {
+    const key = SDL_MapRGBA(
+      _surfaceHandle.format, 
+      mask.r, 
+      mask.g, 
+      mask.b, 
+      mask.a
     );
-    this.setColorKey(
-      enabled, 
-      key
-    );
+    this.setColorKey(enabled, key);
   }
 
   /**
    * Perform a fast surface copy to a destination surface.
   **/
   void blit(
-    SurfaceHandler source, 
-    Rect srcRect, 
-    Rect dstRect
+    SDL_Surface* source, 
+    SDL_Rect srcRect, 
+    SDL_Rect dstRect
   ) @trusted {
     if (!_locked) {
-      SurfaceUtil.self.blitSurface(
-        source,
-        _surfaceHandle,
-        srcRect,
-        dstRect
+      const res = SDL_BlitSurface(
+        source, 
+        &srcRect, 
+        _surfaceHandle, 
+        &dstRect
       );
+      if (res != SDL_FALSE) {
+        Logger.self.error(
+          "Failed to blit surface",
+          typeof(this).stringof
+        );
+      }
     } else {
       Logger.self.warning(
         "The surface is locked. Function call does noething",
@@ -276,17 +330,23 @@ final class Surface {
    * Perform a scaled surface copy to a destination surface.
   **/
   void blitScaled(
-    SurfaceHandler source, 
-    Rect srcRect, 
-    Rect dstRect
+    SDL_Surface* source, 
+    SDL_Rect srcRect, 
+    SDL_Rect dstRect
   ) @trusted {
     if (!_locked) {
-      SurfaceUtil.self.blitScaled(
-        source,
-        _surfaceHandle,
-        srcRect,
-        dstRect
+      const state = SDL_BlitScaled(
+        source, 
+        &srcRect, 
+        _surfaceHandle, 
+        &dstRect
       );
+      if (state != SDL_FALSE) {
+        Logger.self.error(
+          "Failed to blit scaled",
+          typeof(this).stringof
+        );
+      }
     } else {
       Logger.self.warning(
         "The surface is locked. Function call does noething",
@@ -298,7 +358,7 @@ final class Surface {
   /**
    * Returns a handle to the current surface.
   **/
-  package SurfaceHandler getHandle() pure nothrow @safe {
+  package SDL_Surface* getHandle() pure nothrow @safe {
     return _surfaceHandle;
   }
 }
