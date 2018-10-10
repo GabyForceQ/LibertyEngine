b99fcba17fdb214b687b843e09abb668db190919
gabriel.gheorghe <knoppy273@live.com>
2018-10-10 21:46:21 +0300

    Remove all _ from source description

--- source/liberty/core/math/vector.d
+++ source/liberty/core/math/vector.d
@@ -2,7 +2,7 @@
  * Copyright:       Copyright (C) 2018 Gabriel Gheorghe, All Rights Reserved
  * Authors:         $(Gabriel Gheorghe)
  * License:         $(LINK2 https://www.gnu.org/licenses/gpl-3.0.txt, GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007)
- * Source:          $(LINK2 https://github.com/GabyForceQ/LibertyEngine/blob/master/source/liberty/core/math/vector.d, _vector.d)
+ * Source:          $(LINK2 https://github.com/GabyForceQ/LibertyEngine/blob/master/source/liberty/core/math/vector.d)
  * Documentation:
  * Coverage:
 **/
