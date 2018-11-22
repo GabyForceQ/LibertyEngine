c105159cc6736099fc6cd86d33fb85a66163b71b
gabriel.gheorghe <knoppy273@live.com>
2018-11-23 01:28:17 +0200

    Add setDefaultExpHeightFogColor to world

--- source/liberty/scene/world.d
+++ source/liberty/scene/world.d
@@ -18,7 +18,7 @@ import liberty.scene.impl;
 final class World {
   enum {
     /**
-     * The default value of exponential height fog is r: 0.5; g:0.8; b:0.8;
+     * The default value of the exponential height fog is r: 0.5; g:0.8; b:0.8;
     **/
     DEFAULT_EXP_HEIGHT_FOG_COLOR = Vector3F(0.5f, 0.8f, 0.8f)
   }
@@ -55,12 +55,12 @@ final class World {
   }
 
   /**
-   * Set the exp height fog of the scene using 3 floats for the color (RGB) in a template stream function.
-   * Assign a value to exp height fog using world.setExpHeightFogColor(r, g, b) or world.setExpHeightFogColor!"="(r, g, b).
-   * Increment exp height fog by value using world.setExpHeightFogColor!"+="(r, g, b).
-   * Decrement exp height fog by value using world.setExpHeightFogColor!"-="(r, g, b).
-   * Multiply exp height fog by value using world.setExpHeightFogColor!"*="(r, g, b).
-   * Divide exp height fog by value using world.setExpHeightFogColor!"/="(r, g, b).
+   * Set the exp height fog color of the scene using 3 floats for the color (RGB) in a template stream function.
+   * Assign a value to exp height fog color using world.setExpHeightFogColor(r, g, b) or world.setExpHeightFogColor!"="(r, g, b).
+   * Increment exp height fog color by value using world.setExpHeightFogColor!"+="(r, g, b).
+   * Decrement exp height fog color by value using world.setExpHeightFogColor!"-="(r, g, b).
+   * Multiply exp height fog color by value using world.setExpHeightFogColor!"*="(r, g, b).
+   * Divide exp height fog color by value using world.setExpHeightFogColor!"/="(r, g, b).
    * Returns reference to this so it can be used in a stream.
   **/
   World setExpHeightFogColor(string op = "=")(float r, float g, float b) pure nothrow
@@ -70,12 +70,12 @@ final class World {
   }
 
   /**
-   * Set the exp height fog of the scene using a vector of 3 values for the color (RGB) in a template stream function.
-   * Assign a value to exp height fog using world.setExpHeightFogColor(vector3) or world.setExpHeightFogColor!"="(vector3).
-   * Increment exp height fog by value using world.setExpHeightFogColor!"+="(vector3).
-   * Decrement exp height fog by value using world.setExpHeightFogColor!"-="(vector3).
-   * Multiply exp height fog by value using world.setExpHeightFogColor!"*="(vector3).
-   * Divide exp height fog by value using world.setExpHeightFogColor!"/="(vector3).
+   * Set the exp height fog color of the scene using a vector of 3 values for the color (RGB) in a template stream function.
+   * Assign a value to exp height fog color using world.setExpHeightFogColor(vector3) or world.setExpHeightFogColor!"="(vector3).
+   * Increment exp height fog color by value using world.setExpHeightFogColor!"+="(vector3).
+   * Decrement exp height fog color by value using world.setExpHeightFogColor!"-="(vector3).
+   * Multiply exp height fog color by value using world.setExpHeightFogColor!"*="(vector3).
+   * Divide exp height fog color by value using world.setExpHeightFogColor!"/="(vector3).
    * Returns reference to this so it can be used in a stream.
   **/
   World setExpHeightFogColor(string op = "=")(Vector3F expHeightFogColor) pure nothrow {
@@ -88,5 +88,14 @@ final class World {
   **/
   Vector3F getExpHeightFogColor() pure nothrow const {
     return expHeightFogColor;
+  }
+
+  /**
+   * Set exp height fog color to default value $(D DEFAULT_EXP_HEIGHT_FOG_COLOR).
+   * Returns reference to this so it can be used in a stream.
+  **/
+  World setDefaultExpHeightFogColor() pure nothrow {
+    expHeightFogColor = DEFAULT_EXP_HEIGHT_FOG_COLOR;
+    return this;
   }
 }
