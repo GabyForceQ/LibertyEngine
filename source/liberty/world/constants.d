a12f6f4e3de536ba25fadca0f224e8284465465e
gabriel.gheorghe <knoppy273@live.com>
2018-11-26 18:19:12 +0200

    Fix indentation - world.constants

--- source/liberty/world/constants.d
+++ source/liberty/world/constants.d
@@ -11,8 +11,8 @@ module liberty.world.constants;
 import liberty.math.vector;
 
 enum {
-    /**
-     * The default value of the exponential height fog is r: 0.5; g:0.8; b:0.8;
-    **/
-    WORLD_DEFAULT_EXP_HEIGHT_FOG_COLOR = Vector3F(0.5f, 0.8f, 0.8f)
-  }
+  /**
+   * The default value of the exponential height fog is r: 0.5; g:0.8; b:0.8;
+  **/
+  WORLD_DEFAULT_EXP_HEIGHT_FOG_COLOR = Vector3F(0.5f, 0.8f, 0.8f)
+}
