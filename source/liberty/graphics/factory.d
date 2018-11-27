71dbd2d53d33859cef8f0f013aff3e13c5594a1f
gabriel.gheorghe <knoppy273@live.com>
2018-11-27 14:50:10 +0200

    Fix gfx factory package

--- source/liberty/graphics/factory.d
+++ source/liberty/graphics/factory.d
@@ -2,11 +2,11 @@
  * Copyright:       Copyright (C) 2018 Gabriel Gheorghe, All Rights Reserved
  * Authors:         $(Gabriel Gheorghe)
  * License:         $(LINK2 https://www.gnu.org/licenses/gpl-3.0.txt, GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007)
- * Source:          $(LINK2 https://github.com/GabyForceQ/LibertyEngine/blob/master/source/liberty/graphics/factory/renderer.d)
+ * Source:          $(LINK2 https://github.com/GabyForceQ/LibertyEngine/blob/master/source/liberty/graphics/factory.d)
  * Documentation:
  * Coverage:
 **/
-module liberty.graphics.factory.renderer;
+module liberty.graphics.factory;
 
 version (__OPENGL__)
   import bindbc.opengl;
